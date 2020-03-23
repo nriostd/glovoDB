4f66596d80b1192b87c21192c11c340d1575c87f
Renato Afonso <renato.afonso@talkdesk.com>
2020-03-23 16:38:13 +0000

    remove token and logs

--- app/views.py
+++ app/views.py
@@ -3,7 +3,6 @@ from app import app
 from .utils.numbers import isValidNumber, JSONEncoder
 from os import environ
 import re
-#from models import Number
 
 import json, re
 from flask_pymongo import PyMongo
@@ -11,69 +10,43 @@ from flask_pymongo import PyMongo
 client = PyMongo(app)
 db = client.db.numbers
 
-
-ACCESS_TOKEN = environ.get('ACCESS_TOKEN')
-
-
 @app.route('/api/blacklist', methods =['POST', 'GET', 'DELETE'])
 def Number():
     if request.method == 'POST':
-        pre_token = str(request.headers.get('Authorization'))
-        print(pre_token)
-        token = pre_token.replace('Basic ','')
         td_number = str(request.json.get('talkdesk_number'))
         b_number = str(request.json.get('blacklist_number'))
-        print(token)
-        print(td_number)
-        print(b_number)
-        if (token == None) or (td_number == None) or (b_number == None) or (isValidNumber(b_number) == False) or (isValidNumber(td_number) == False):
+        if (td_number == None) or (b_number == None) or (isValidNumber(b_number) == False) or (isValidNumber(td_number) == False):
             return jsonify({'error': 'Invalid request'}),400 #invalid request
-        elif token != ACCESS_TOKEN:
-            return jsonify({'error': 'Unauthorized request'}),401 #unauthorized
         else:
             already_present = db.find_one({"blacklist_number": b_number, "talkdesk_number": td_number})
             if already_present == None:
                 inserted = db.insert_one({"blacklist_number": b_number, "talkdesk_number": td_number})
                 result_id = JSONEncoder().encode(inserted.inserted_id)
                 result_id = re.sub('[^0-9a-zA-Z]+', '', result_id)
-                print(result_id)
                 return jsonify({'id':result_id}),200 #successfully inserted
             return jsonify({'error': 'Already present.'}),400 #blacklist number already reported
 
 
     if request.method == 'GET':
-        pre_token = str(request.headers.get('Authorization'))
-        print(pre_token)
-        token = pre_token.replace('Basic ','')
         td_number = str(request.args.get('talkdesk_number'))
         td_number = re.sub(' ', '+', td_number)
-        print(td_number)
         b_number = str(request.args.get('blacklist_number'))
         b_number = re.sub(' ', '+', b_number)
-        print(b_number)
-        if (token == None) or (td_number == None) or (b_number == None) or (isValidNumber(b_number) == False) or (isValidNumber(td_number) == False):
+        if (td_number == None) or (b_number == None) or (isValidNumber(b_number) == False) or (isValidNumber(td_number) == False):
             return jsonify({'error': 'Invalid request'}),400 #invalid request
-        elif token != ACCESS_TOKEN:
-            return jsonify({'error': 'Unauthorized request'}),401 #unauthorized
         else:
             query = {"blacklist_number": b_number, "talkdesk_number": td_number}
             found = db.find_one(query)
             if found != None:
                 found_one = json.loads(JSONEncoder().encode(found))
-                print(found_one)
                 return json.loads(JSONEncoder().encode(found)),200
             return jsonify({'error': 'Number not found.'}),404
 
     if request.method == 'DELETE':
-        pre_token = str(request.headers.get('Authorization'))
-        print(pre_token)
-        token = pre_token.replace('Basic ','')
         td_number = str(request.headers.get('talkdesk_number'))
         b_number = str(request.headers.get('blacklist_number'))
-        if (token == None) or (td_number == None) or (b_number == None) or (isValidNumber(b_number) == False) or (isValidNumber(td_number) == False):
+        if (td_number == None) or (b_number == None) or (isValidNumber(b_number) == False) or (isValidNumber(td_number) == False):
             return jsonify({'error': 'Invalid request'}),400 #invalid request
-        elif token != ACCESS_TOKEN:
-            return jsonify({'error': 'Unauthorized request'}),401 #unauthorized
         else:
             query = {"blacklist_number": b_number, "talkdesk_number": td_number}
             result = db.delete_one(query)
@@ -85,13 +58,8 @@ def Number():
 
 @app.route('/api/blacklist/all', methods = ['GET'])
 def api_all():
-    pre_token = str(request.headers.get('Authorization'))
-    print(pre_token)
-    token = pre_token.replace('Basic ','')
     if token is None:
         return jsonify({'error': 'Invalid request'}),400
-    elif token != ACCESS_TOKEN:
-        return jsonify({'error': 'Unauthorized request'}),401
     else:
         json_dict = {'blacklist_numbers': [], 'total_numbers': 0}
         for doc in db.find():
