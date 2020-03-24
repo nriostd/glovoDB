6d6d657afde9600c0d715861f99ebef3a48c3d5a
Renato Afonso <renato.afonso@talkdesk.com>
2020-03-24 17:30:19 +0000

    fix param from get

--- app/views.py
+++ app/views.py
@@ -41,10 +41,9 @@ def Number():
         if (token == None) or (token != ACCESS_TOKEN):
             logging.error('Token missing or wrong token')
             return jsonify({'error': 'Unauthorized request'}),401
-        td_number = str(request.headers.get('talkdesk_number'))
-        logging.info(td_number)
+        td_number = str(request.args.get('talkdesk_number'))
         td_number = re.sub(' ', '+', td_number)
-        b_number = str(request.headers.get('blacklist_number'))
+        b_number = str(request.args.get('blacklist_number'))
         b_number = re.sub(' ', '+', b_number)
         if (td_number == None) or (b_number == None) or (isValidNumber(b_number) == False) or (isValidNumber(td_number) == False):
             return jsonify({'error': 'Invalid request'}),400 #invalid request
