21ef02787a68d78f6a4a2efcc0b733fff25fbc15
Renato Afonso <renato.afonso@talkdesk.com>
2020-03-25 16:58:17 +0000

    add number regex

--- app/views.py
+++ app/views.py
@@ -21,7 +21,9 @@ def Number():
             logging.error('Token missing or wrong token')
             return jsonify({'error': 'Unauthorized request'}),401
         td_number = str(request.json.get('talkdesk_number'))
+        td_number = re.sub(' ', '+', td_number)
         b_number = str(request.json.get('blacklist_number'))
+        b_number = re.sub(' ', '+', b_number)
         if (td_number == None) or (b_number == None) or (isValidNumber(b_number) == False) or (isValidNumber(td_number) == False):
             return jsonify({'error': 'Invalid request'}),400 #invalid request
         else:
@@ -59,7 +61,9 @@ def Number():
             logging.error('Token missing or wrong token')
             return jsonify({'error': 'Unauthorized request'}),401
         td_number = str(request.headers.get('talkdesk_number'))
+        td_number = re.sub(' ', '+', td_number)
         b_number = str(request.headers.get('blacklist_number'))
+        b_number = re.sub(' ', '+', b_number)
         if (td_number == None) or (b_number == None) or (isValidNumber(b_number) == False) or (isValidNumber(td_number) == False):
             return jsonify({'error': 'Invalid request'}),400 #invalid request
         else:
